fa8a2a62cd7c0e3f3d52ad6097675e1119eb3c1d
Lucas Neves <62020657+LucasNeves1@users.noreply.github.com>
2023-10-23 21:43:22 -0300

    [UPDATE] Criação do CRUD para marcas

---
+++ resources/views/marcas.blade.php
@@ -0,0 +1,33 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Lista de marcas') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <form method="POST" action="{{ route('login') }}">
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="nome" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+    </form>
+        </div>
+    </div>
+</x-app-layout>
